8a187147a1a3d5a4b96149e63f6642092afc91ea
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-09-11 20:53:38 +0700

    Update neon.js

--- plugins/neon.js
+++ plugins/neon.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('Proses...')
-  let res = `https://api.lolhuman.xyz/api/textprome/neon?nama=${response[0]}&apikey=LolHuman`
+  let res = `https://api.lolhuman.xyz/api/textprome/neon?apikey=07c1c8f96a419c66e1ed83e2&text=LoLHuman`
   conn.sendFile(m.chat, res, 'neon.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['neon'].map(v => v + ' <text>')
-handler.tags = ['textprome']
+handler.tags = ['maker']
 handler.command = /^(neon)$/i
 
 export default handler
