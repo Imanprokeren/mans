ed32c69c95281a73c3c448cfe6052c5a6e145866
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-09-11 21:56:43 +0700

    Add files via upload

---
+++ plugins/logo-lighttext.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+   let response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Teks'
+  m.reply('Proses...')
+  let res = `https://api.lolhuman.xyz/api/ephoto1/lighttext?apikey=941dce43fc0a344ceb536d7a&text=${response}`
+  conn.sendFile(m.chat, res, 'lighttext.jpg', `Sudah Jadi`, m, false)
+}
+handler.help = ['lighttext'].map(v => v + ' <text>')
+handler.tags = ['maker']
+handler.command = /^(lighttext)$/i
+
+export default handler
