a5c59bcdb528962e3854396334c3c986e3b93938
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-09-11 12:09:43 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,23 +2,23 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
+let str = `*https://www.youtube.com/c/ManzBotEdtz12*
 
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
+Pengen Scriptnya?
+Nantikan di channel Owner ya 😇
+https://www.youtube.com/c/ManzBotEdtz12
 
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
+Oh ya, Script ini dijual loh!
+Mau beli? Langsung aja chat nomor Owner!
 
-wa.me/6282127487538`
+wa.me/6289522724388`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/Imannn17n",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'IMAN-BOT-V1 MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
