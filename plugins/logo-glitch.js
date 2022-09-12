1a733002e90c6750b87a31bd38edfe905a81ae91
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-09-12 19:15:00 +0700

    Update logo-glitch.js

--- plugins/logo-glitch.js
+++ plugins/logo-glitch.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Teksnya!'
   m.reply('proses..')
-  let res = `https://api.lolhuman.xyz/api/textprome2/glitch?apikey=6e551d9967ff8fe559a7aaa4&text1={response}&text2={response}`
+  let res = `https://api.lolhuman.xyz/api/textprome2/glitch?apikey=6e551d9967ff8fe559a7aaa4&text1=${response[0]}&text2=${response[1]}`
   conn.sendFile(m.chat, res, 'glitch.jpg', `Nih kak`, m, false)
 }
 handler.help = ['glitch'].map(v => v + ' <text|text>')
